6b104799530078d1348465c0352f7af6cdfc8194
Finn Thye <piqueen@gmail.com>
2018-08-10 12:40:57 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import map from 'lodash/map';
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+import toLower from 'lodash/toLower';
+
+const Sentence = styled.p`
+  color : ${(props) => props.color };
+  font-size : 16px;
+`
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters : []
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://www.potterapi.com/v1/characters/?key=%242a%2410%245wOJEuLpDgrj4Q4Ah0K9luTqacT518KBYUz5CIud2HDlJZIKFiXwy&house=Gryffindor';
+
+    fetch(url)
+    .then((res) => {
+      if (res.status >= 200 && res.status < 300) {
+        return res.json();
+      }
+      console.warn('Fails to obtain characters', res);
+      return undefined;
+    })
+    .then((characters) => {
+      if (!characters) {
+        return undefined;
+      }
+      this.setState( { characters } );
+    })
+  }
+
+  render() {
+    console.log(this.state.characters);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          {map(this.state.characters, (character) => ( 
+            <Sentence key={character._id} color={toLower(character.role) === 'student' ? 'red' : 'black'} >
+              {character.name} is a {character.bloodStatus}
+            </Sentence>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
