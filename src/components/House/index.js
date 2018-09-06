80955fbb51d79d44e4f0f0608bb17dd347ae11b0
Finn Thye <piqueen@gmail.com>
2018-09-06 10:00:48 -0600

    Kelson helped me finish the House card and get map to work

--- src/components/House/index.js
+++ src/components/House/index.js
@@ -9,7 +9,10 @@ class House extends Component {
   }
 
   componentDidMount() {
-    const url = 'https://www.potterapi.com/v1/houses/5a05e2b252f721a3cf2ea33f?key=%242a%2410%245wOJEuLpDgrj4Q4Ah0K9luTqacT518KBYUz5CIud2HDlJZIKFiXwy';
+    const houseID = '5a05e2b252f721a3cf2ea33f';
+    const APIkey = '%242a%2410%245wOJEuLpDgrj4Q4Ah0K9luTqacT518KBYUz5CIud2HDlJZIKFiXwy';
+
+    const url = `https://www.potterapi.com/v1/houses/${houseID}?key=${APIkey}`;
 
     fetch(url)
       .then((res) => {
@@ -27,60 +30,23 @@ class House extends Component {
       });
   }
 
-  getKeys() {
-    let keyArray = Object.keys(this.state.house).forEach((key) => {
-      console.log('key: ', key);
-
-      return key;
-    }, this);
-    return keyArray;
-  }
-
-  getValues() {
-    let valueArray = Object.values(this.state.house).forEach((value) => {
-      console.log('value: ', value);
-
-      return value;
-    }, this);
-    return valueArray;
-  }
-
   render() {
     console.log(this.state.house);
 
     return (
       <div className="house">
-        <h1> Here are the keys: {this.getKeys()} </h1>
-        <h2> Here are the values: {this.getValues()} </h2>
-        {this.state.house.name} dearly holds these values to be sacred: {map(this.state.house.values, value => (
-          <div className="value" key="value"> 
-            {house.values[0]}
-          </div>
-        ))}
+        <div className="house-name">{this.state.house.name}</div>
+        <div className="phrase">dearly holds these values to be sacred: </div>
+        <div className="house-k-v-pairs">
+          {map(this.state.house, (value, key) => (
+            <div className="value" key={key}>
+              {key} : {JSON.stringify(value)}
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
 export default House;
-
-/*       {map(this.state.houses, house => (
-          <div className="house" key="house._id">
-            <div className="name">
-              {house.name}
-            </div>
-            <div className="mascot">
-              {house.mascot}
-            </div>
-            <div className="headOfHouse">
-              {house.headOfHouse}
-            </div>
-            <div className="houseGhost">
-              { house.houseGhost }
-            </div>
-            <div className="values">
-              {house.values}
-            </div>
-          </div>
-        ))}
-*/
