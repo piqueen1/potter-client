c47f4f05a794ce1ea943cfc7f38af5fc346086d5
Finn Thye <piqueen@gmail.com>
2018-10-11 05:55:11 -0600

    Building out Characters fetch

--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Map from 'lodash/map';
+import { url } from 'inspector';
 
 class Characters extends Component {
   constructor(props) {
@@ -9,9 +10,28 @@ class Characters extends Component {
     }
   }
 
+  componentDidMount(){
+    const URL = 'https://www.potterapi.com/v1/houses?key=$2a$10$5wOJEuLpDgrj4Q4Ah0K9luTqacT518KBYUz5CIud2HDlJZIKFiXwy';
 
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 300) {
+          return res.json();
+        }
+        console.warn('Voldemort struck!  No characters for you!')
+        return undefined;
+      })
+      .then ((houses) => {
+        if (!houses) {
+          return undefined;
+        }
+        this.setState({ houses });
+      });
+  }
 
   render(
 
   )
 }
+
+export default Characters;
